86d8082a4ce09220dfdbc78fe4d03898a1e408e9
volt-l18 <ng05031975@gmail.com>
2024-12-29 19:45:29 +0530

    basic front end is ready ( i used v0 to make it >.< )

--- Site/main/routes.py
+++ Site/main/routes.py
@@ -6,7 +6,3 @@ main = Blueprint('main' , __name__)
 @main.route("/home")
 def home():
 	return render_template('home.html')
-
-@main.route("/about")
-def about():
-	return render_template('about.html' , title = "About")
