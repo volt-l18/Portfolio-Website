ab1b2ec046440d0222fc0ffda53042da82246a0e
volt-l18 <ng05031975@gmail.com>
2025-01-19 21:04:55 +0530

    did some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
+import Content from "./components/content";
 import { useState } from "react";
 
 function App() {
-  let items = ["Name : khushal Neekhra"];
-
-  const heandleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  //
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
       <Navbar />
-      <ListGroup
-        items={items}
-        heading="hello"
-        onSelectItem={heandleSelectItem}
-      />
+      <Content />
       {alertVisible && (
         <Alert onclose={() => setAlertVisibility(false)}>
           good now hire me <br /> Email :
