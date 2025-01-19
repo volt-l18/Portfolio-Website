189be57c49b85f5c9cd220e015cb8a72d0988c87
volt-l18 <ng05031975@gmail.com>
2025-01-19 22:24:33 +0530

    scraped everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import Navbar from "./components/Navbar";
-import Content from "./components/content";
-import { useState } from "react";
+import "./App.css";
 
 function App() {
-  //
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
-    <div>
-      <Navbar />
-      <Content />
-      {alertVisible && (
-        <Alert onclose={() => setAlertVisibility(false)}>
-          good now hire me <br /> Email :
-          {
-            <a
-              target="_blank"
-              href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDZcmFDJbgkRSxKdzfbBPMxJlSpcgmkrsqGTmRlshxNjpnLgdJVgcxbCQSKJnqMkQVjGkdC"
-            >
-              khushalneekhr@gmail.com
-            </a>
-          }
-        </Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)} color="info">
-        click me
-      </Button>
-    </div>
+    <>
+      <div className="">
+        <h1 className="text-3xl font-bold ">Hello i'm Khushal 👋</h1>
+      </div>
+    </>
   );
 }
 
