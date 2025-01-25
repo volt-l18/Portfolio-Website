af049e8c603b66dc8c2f4b0f5ab437210f98996f
volt-l18 <ng05031975@gmail.com>
2025-01-25 18:06:15 +0530

    added skills.tsx, skilcard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skils";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <main>
         <Hero />
         <About />
+        <Skills />
       </main>
     </>
   );
---
+++ src/components/Skils.tsx
@@ -0,0 +1,83 @@
+/*
+ * Components
+ */
+import SkillCard from "./SkillCard";
+
+const skillItem = [
+  {
+    imgSrc: "/images/python.svg",
+    label: "Python",
+    desc: "Genral purpose/Logic building",
+  },
+  {
+    imgSrc: "/images/flask.svg",
+    label: "Flask",
+    desc: "Framework",
+  },
+  {
+    imgSrc: "/images/mysql.svg",
+    label: "SQL",
+    desc: "Database",
+  },
+  {
+    imgSrc: "/images/godot.svg",
+    label: "Godot",
+    desc: "Game development",
+  },
+  {
+    imgSrc: "/images/blender.svg",
+    label: "Blender",
+    desc: "3D assets creation",
+  },
+  {
+    imgSrc: "/images/css3.svg",
+    label: "CSS",
+    desc: "User Interface",
+  },
+  {
+    imgSrc: "/images/javascript.svg",
+    label: "JavaScript",
+    desc: "Interaction",
+  },
+  {
+    imgSrc: "/images/typescript.svg",
+    label: "TypeScript",
+    desc: "Interaction",
+  },
+  {
+    imgSrc: "/images/nodejs.svg",
+    label: "NodeJS",
+    desc: "Web Server",
+  },
+  {
+    imgSrc: "/images/react.svg",
+    label: "React",
+    desc: "Framework",
+  },
+  {
+    imgSrc: "/images/tailwindcss.svg",
+    label: "TailwindCSS",
+    desc: "User Interface",
+  },
+];
+
+const Skills = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h2 className="headline-2">Essential Tools I use</h2>
+        <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+          Discover the powerful tools and technologies I use to create
+          exceptional, high-performing websites & applications.
+        </p>
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+          {skillItem.map(({ imgSrc, label, desc }, key) => (
+            <SkillCard imgSrc={imgSrc} label={label} desc={desc} key={key} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
