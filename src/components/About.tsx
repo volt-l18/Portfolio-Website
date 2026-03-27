936c9734bb1b1ba6ce7ee170ab07f1f0ec5332b8
volt-l18 <ng05031975@gmail.com>
2026-03-27 21:22:49 +0530

    updated the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,9 @@ const About = () => {
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-2 md:mb-2 md:text-2xl ">
-            I'm a React and Tailwind developer with a passion for building
-            user-centric, responsive UIs. I have a strong grasp of TypeScript
-            for scalable and maintainable code, along with Python expertise for
-            backend development and automation. I love building simple,
-            intuitive interfaces and automating tasks with code.
+             <span className="font-bold block mb-6">Full-Stack Developer | AI Explorer | Git Expert</span>
+              I am a versatile Software Developer focused on building high-performance web apps and immersive digital worlds. With a foundation in  <span className="font-bold">React, Flask, and TypeScript</span>, I bridge the gap between technical excellence and creative problem-solving. <span className="block mb-6"></span>
+              An expert command of  <span className="font-bold">Git and GitHub</span> is maintained for version control and collaboration. Advanced AI tools like  <span className="font-bold">Gemini CLI, Claude Code, and Ollama</span> are used to build smarter, more efficient solutions. The goal is to deliver scalable, secure, and user-centric software, from securing user data to designing platformers like Slimy Tails.
           </p>
         </div>
       </div>
